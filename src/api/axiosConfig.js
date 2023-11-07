2a8b7dec636515895c88b25fb271cb6cb2abc3e7
Rayy-007 <minkhant88887@gmail.com>
2023-11-07 10:09:32 +0630

    Fixed Bug for Fetching API

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://9c96-103-106-239-104.ap.ngrok.io",
+  baseURL: "https://79b2-103-121-230-173.ngrok-free.app",
   headers: { "ngrok-skip-browser-warning": "true" },
 });
